edd67a71d441a6ca592dbe5fbf541374d4b3a7d2
lluiscab <lluiscab@gmail.com>
2018-08-26 15:06:34 +0200

    Added block drops and fix indentation

--- generate_enums.js
+++ generate_enums.js
@@ -1,13 +1,13 @@
-var fs = require('fs');
-var assert = require('assert');
-var Batch = require('batch');
-var path = require('path');
+var fs = require('fs')
+var assert = require('assert')
+var Batch = require('batch')
+var path = require('path')
 
-var burgerJsonPath = process.argv[2];
+var burgerJsonPath = process.argv[2]
 
-if (! burgerJsonPath) {
-  console.error("Usage: node generate_enums.js burger.json");
-  process.exit(1);
+if (!burgerJsonPath) {
+  console.error('Usage: node generate_enums.js burger.json')
+  process.exit(1)
 }
 
 var outputs = [
@@ -15,103 +15,136 @@ var outputs = [
   blocks,
   biomes,
   recipes
-];
+]
 
-fs.readFile(burgerJsonPath, 'utf8', function(err, json) {
-  assert.ifError(err);
-  var batch = new Batch();
-  outputs.forEach(function(output) {
-    batch.push(function(cb) {
-      output(JSON.parse(json), cb);
-    });
-  });
-  batch.end(function(err, results) {
-    assert.ifError(err);
-    var batch = new Batch();
-    results.forEach(function(result) {
-      batch.push(function(cb) {
-        var filepath = path.join(__dirname, 'enums', result.name + '.json');
-        var serialised = JSON.stringify(result.json, null, 2);
-        fs.writeFile(filepath, serialised, cb);
-      });
-    });
-    batch.end(function(err) {
-      assert.ifError(err);
-    });
-  });
-});
+fs.readFile(burgerJsonPath, 'utf8', function (err, json) {
+  if (err) return console.error(err)
+  assert.ifError(err)
+  var batch = new Batch()
+  outputs.forEach(function (output) {
+    batch.push(function (cb) {
+      output(JSON.parse(json), cb)
+    })
+  })
+  batch.end(function (err, results) {
+    if (err) return console.error(err)
+    assert.ifError(err)
+    var batch = new Batch()
+    results.forEach(function (result) {
+      batch.push(function (cb) {
+        var filepath = path.join(__dirname, 'enums', result.name + '.json')
+        var serialised = JSON.stringify(result.json, null, 2)
+        fs.writeFile(filepath, serialised, cb)
+      })
+    })
+    batch.end(function (err) {
+      if (err) return console.error(err)
+      assert.ifError(err)
+    })
+  })
+})
 
-function items(burger, cb) {
-  var itemsJson = burger[0].items.item;
-  var item;
-  var itemEnum = [];
+function items (burger, cb) {
+  var itemsJson = burger[0].items.item
+  var item
+  var itemEnum = []
   for (var id in itemsJson) {
-    item = itemsJson[id];
+    item = itemsJson[id]
     itemEnum.push({
       id: item.numeric_id,
       textId: item.text_id,
       displayName: item.display_name,
       name: item.text_id,
       stackSize: item.max_stack_size
-    });
+    })
   }
   cb(null, {
     name: 'items',
     json: itemEnum
-  });
+  })
 }
 
-function blocks(burger, cb) {
-  var blocksJson = burger[0].blocks.block;
-  var block;
+function blocks (burger, cb) {
+  var blocksJson = burger[0].blocks.block
+  var block
   var blockEnum = [
     {
       id: 0,
-      name: "air",
-      displayName: "Air",
-      hardness: 0
+      name: 'air',
+      displayName: 'Air',
+      hardness: 0,
+      diggable: true, // Missing
+      boundingBox: 'empty', // Missing, could get from the block model maybe
+      drops: [ 0 ],
+      transparent: true, // Missing
+      emitLight: 0, // Missing
+      filterLight: 0 // Missing
     }
-  ];
+  ]
   for (var id in blocksJson) {
-
-    block = blocksJson[id];
-    let states = [];
-    if(block.states) {
-
-        for(var index in block.states) {
-            let state = block.states[index];
-            states.push({
-                name: state.name,
-                type: state.type,
-                values: state.values,
-                num_values: state.num_values
-            })
-        }
-
+    block = blocksJson[id]
+    let states = []
+    if (block.states) {
+      for (var index in block.states) {
+        let state = block.states[index]
+        states.push({
+          name: state.name,
+          type: state.type,
+          values: state.values,
+          num_values: state.num_values
+        })
+      }
     }
-    
+
+    let drops = null
+
+    let idParsed = block.text_id.replace('wall_', '')
+    if (burger[0].items.item[idParsed]) {
+      drops = burger[0].items.item[idParsed].text_id
+    }
+
+    if (!drops) {
+      if (idParsed === 'potatoes') drops = [ 'potato' ]
+      if (idParsed === 'redstone_wire') drops = [ 'redstone' ]
+      if (idParsed === 'tripwire') drops = [ 'string' ]
+      if (idParsed === 'melon_stem') drops = [ 'melon_seeds' ]
+      if (idParsed === 'pumpkin_stem') drops = [ 'pumpkin_seeds' ]
+      if (idParsed === 'beetroots') drops = [ 'beetroot' ]
+      if (idParsed === 'carrots') drops = [ 'carrot' ]
+      if (idParsed === 'cocoa') drops = [ 'cocoa_beans' ]
+      if (idParsed === 'kelp_plant') drops = [ 'kelp' ]
+      if (idParsed === 'tall_seagrass') drops = [ 'seagrass' ]
+    }
+
+    if (!drops && idParsed.startsWith('potted')) {
+      drops = [
+        'flower_pot',
+        idParsed.substring(7)
+      ]
+    }
+
     blockEnum.push({
       id: block.numeric_id,
       textId: block.text_id,
       displayName: block.display_name,
       name: block.name,
+      drops: drops ? [ drops ] : [],
       hardness: block.hardness,
       states: states
-    });
-    
+    })
   }
   cb(null, {
     name: 'blocks',
     json: blockEnum
-  });
+  })
 }
 
-function biomes(burger, cb) {
-  var biomesJson = burger[0].biomes.biome;
-  var biome;
-  var biomeEnum = [];
+function biomes (burger, cb) {
+  var biomesJson = burger[0].biomes.biome
+  var biome
+  var biomeEnum = []
   for (var name in biomesJson) {
-    biome = biomesJson[name];
+    biome = biomesJson[name]
     biomeEnum.push({
       id: biome.id,
       color: biome.color,
@@ -119,64 +152,66 @@ function biomes(burger, cb) {
       name: biome.name,
       rainfall: biome.rainfall,
       temperature: biome.temperature
-    });
+    })
   }
   cb(null, {
     name: 'biomes',
     json: biomeEnum
-  });
+  })
 }
 
-function recipes(burger, cb) {
-    
-  var recipesJson = burger[0].recipes;
-  var recipeEnum = {};
-  var recipeItemList, recipeItem, ingredients;
+function recipes (burger, cb) {
+  var recipesJson = burger[0].recipes
+  var recipeEnum = {}
+  var recipeItemList, ingredients
 
   for (var makesId in recipesJson) {
+    var recipeList = recipesJson[makesId]
+    recipeEnum[makesId] = recipeItemList = []
+    var j, shape, shapeLine
 
-    var recipeList = recipesJson[makesId];
-    recipeEnum[makesId] = recipeItemList = [];
-    var j, shape, shapeLine;
-    
     for (var i = 0; i < recipeList.length; ++i) {
-
-      var recipe = recipeList[i];
+      var recipe = recipeList[i]
       if (recipe.type === 'shape') {
-
         recipeItemList.push({
           count: recipe.amount,
           metadata: recipe.metadata,
-          inShape: shape = []
-        });
+          inShape: shape = [],
+          result: {
+            count: recipe.makes.count,
+            textId: recipe.makes.name
+          }
+        })
         for (j = recipe.shape.length - 1; j >= 0; --j) {
-          var line = recipe.shape[j];
-          shape.push(shapeLine = []);
+          var line = recipe.shape[j]
+          shape.push(shapeLine = [])
           for (var k = 0; k < line.length; ++k) {
-            shapeLine.push(line[k] || null);
+            if (line[k]) shapeLine.push(line[k].name)
+            else shapeLine.push(null)
           }
         }
-        
       } else if (recipe.type === 'shapeless') {
-
         recipeItemList.push({
           count: recipe.amount,
           metadata: recipe.metadata,
-          ingredients: ingredients = []
-        });
-        
-        for(j = 0; j < recipe.ingredients.length; ++j) {
-          var ingredient = [j];
-          ingredients.push({ ...ingredient });
+          ingredients: ingredients = [],
+          result: {
+            count: recipe.makes.count,
+            textId: recipe.makes.name
+          }
+        })
+
+        for (j = 0; j < recipe.ingredients.length; ++j) {
+          var ingredient = recipe.ingredients[j]
+          ingredients.push(ingredient.name)
         }
-        
       } else {
-        throw new Error("unexpected recipe type: " + recipe.type)
+        throw new Error('unexpected recipe type: ' + recipe.type)
       }
     }
   }
   cb(null, {
-    name: "recipes",
+    name: 'recipes',
     json: recipeEnum
-  });
+  })
 }
