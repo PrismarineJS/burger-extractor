d8e72010399ce2635408de7f9170cb2d03874809
lluiscab <lluiscab@gmail.com>
2018-09-06 11:21:20 +0200

    Better logging

--- src/extractors/blocks.js
+++ src/extractors/blocks.js
@@ -1,101 +1,113 @@
+/*
+    Block extractor
 
-// Block extractor
+    This script does not extract the following data: emitLight, filterLight, boundingBox and transparent
+
+*/
 
 const fs = require('fs')
 const path = require('path')
+const chalk = require('chalk')
 
 module.exports = ({ blocks, items }, outputDirectory) => new Promise((resolve, reject) => {
+    
+  console.log(chalk.green('    Extracing block data'))
   const extracted = []
 
   // Extract data
   for (let name in blocks.block) {
-    const block = blocks.block[name]
 
-    const blockData = {
-      id: block.numeric_id,
-      textId: block.text_id,
-      displayName: block.display_name,
-      name: block.text_id,
-      hardness: block.hardness
-    }
+    // For each block extracted by burger
 
-    // Get block hardness
+        const block = blocks.block[name]
 
-    if (!blockData.hardness) blockData.hardness = 0
+    // Initial block data
 
-    // TODO: Get block emitLight, filterLight, boundingBox and transparent
+        const blockData = {
+            id: block.numeric_id,
+            displayName: block.display_name,
+            name: block.text_id,
+            hardness: block.hardness || 0
+        }
 
     // Get block states
 
-    const states = []
-    if (block.states) {
-      for (var index in block.states) {
-        let state = block.states[index]
-        states.push({
-          name: state.name,
-          type: state.type,
-          values: state.values,
-          num_values: state.num_values
-        })
-      }
-    }
+        const states = []
 
-    blockData.states = states
+        if (block.states) {
+            for (var index in block.states) {
+
+                let state = block.states[index]
+                 states.push({
+                    name: state.name,
+                    type: state.type,
+                    values: state.values,
+                    num_values: state.num_values
+                })
+
+            }
+        }
+
+        blockData.states = states
 
     // Get block drops
 
-    let drops = null
+        let drops = []
 
-    // Remove any `wall_` from block id (skeleton_skull item has two blocks, skeleton_skull and skeleton_wall_skull)
-    let idParsed = block.text_id.replace('wall_', '')
-    if (items.item[idParsed]) {
-      drops = items.item[idParsed].text_id
-    }
+        // Remove any `wall_` from block id (skeleton_skull item has two blocks, skeleton_skull and skeleton_wall_skull)
+        let idParsed = block.text_id.replace('wall_', '')
+        if (items.item[idParsed]) { // If the item exists
+            drops.push(items.item[idParsed].text_id);
+        }
 
-    // Check if the block is a flower pot with a flower in it
-    if (!drops && idParsed.startsWith('potted')) {
-      drops = [
-        'flower_pot',
-        idParsed.substring(7)
-      ]
-    }
+        // Check if the block is a flower pot with a flower in it
+        if (!drops && idParsed.startsWith('potted')) {
+            drops.push('flower_pot');
+            drops.push(idParsed.substring(7))
+        }
 
-    // Manually check for other possible blocks
-    if (!drops) {
-      drops = []
+        // Manually check for other possible blocks
+        if (!drops.length) {
 
-      if (idParsed === 'potatoes') drops.push('potato')
-      if (idParsed === 'redstone_wire') drops.push('redstone')
-      if (idParsed === 'tripwire') drops.push('string')
-      if (idParsed === 'melon_stem') drops.push('melon_seeds')
-      if (idParsed === 'pumpkin_stem') drops.push('pumpkin_seeds')
-      if (idParsed === 'beetroots') drops.push('beetroot')
-      if (idParsed === 'carrots') drops.push('carrot')
-      if (idParsed === 'cocoa') drops.push('cocoa_beans')
-      if (idParsed === 'kelp_plant') drops.push('kelp')
-      if (idParsed === 'tall_seagrass') drops.push('seagrass')
-    }
+            if (idParsed === 'potatoes') drops.push('potato')
+            if (idParsed === 'redstone_wire') drops.push('redstone')
+            if (idParsed === 'tripwire') drops.push('string')
+            if (idParsed === 'melon_stem') drops.push('melon_seeds')
+            if (idParsed === 'pumpkin_stem') drops.push('pumpkin_seeds')
+            if (idParsed === 'beetroots') drops.push('beetroot')
+            if (idParsed === 'carrots') drops.push('carrot')
+            if (idParsed === 'cocoa') drops.push('cocoa_beans')
+            if (idParsed === 'kelp_plant') drops.push('kelp')
+            if (idParsed === 'tall_seagrass') drops.push('seagrass')
 
-    blockData.drops = drops
+        }
+
+        if(!drops.length) {
+
+        }
+
+        blockData.drops = drops
 
     // Get block diggable
 
-    let diggable = true
+        let diggable = true
 
-    if (block.hardness === -1.0) diggable = false
-    if (block.text_id === 'water' || block.text_id === 'lava') diggable = false
+        if (block.hardness === -1.0) diggable = false
+        if (block.text_id === 'water' || block.text_id === 'lava') diggable = false
 
-    blockData.diggable = diggable
+        blockData.diggable = diggable
 
-    extracted.push(blockData)
+    // Push the data    
+        extracted.push(blockData)
+
   }
 
-  // Sort data
+  // Sort data by id
   extracted.sort((a, b) => (a.id - b.id))
 
   try {
     fs.writeFileSync(path.join(outputDirectory, 'blocks.json'), JSON.stringify(extracted, null, 2))
-    resolve()
+    resolve();
   } catch (e) {
     reject(e)
   }
