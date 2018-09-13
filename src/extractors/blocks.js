b878e9250f41e569a3070a586f896d3b89858a49
lluiscab <lluiscab@gmail.com>
2018-09-13 11:18:52 +0200

    Block drops now use the numeric id instead of the text id

--- src/extractors/blocks.js
+++ src/extractors/blocks.js
@@ -12,6 +12,10 @@ module.exports = ({ blocks, items }, outputDirectory) => new Promise((resolve, r
   console.log(chalk.green('    Extracing block data'))
   const extracted = []
 
+  function findItemByName(name) {
+    return items.item[name].numeric_id || null
+  }
+
   // Extract data
   for (let name in blocks.block) {
     // For each block extracted by burger
@@ -48,27 +52,27 @@ module.exports = ({ blocks, items }, outputDirectory) => new Promise((resolve, r
     // Remove any `wall_` from block id (skeleton_skull item has two blocks, skeleton_skull and skeleton_wall_skull)
     let idParsed = block.text_id.replace('wall_', '')
     if (items.item[idParsed]) { // If the item exists
-      drops.push(items.item[idParsed].text_id)
+      drops.push(items.item[idParsed].numeric_id)
     }
 
     // Check if the block is a flower pot with a flower in it
     if (idParsed.startsWith('potted_')) {
-      drops.push('flower_pot')
-      drops.push(idParsed.substring(7))
+      drops.push(findItemByName('flower_pot'))
+      drops.push(findItemByName(idParsed.substring(7)));
     }
 
     // Manually check for other possible blocks
     if (!drops.length) {
-      if (idParsed === 'potatoes') drops.push('potato')
-      if (idParsed === 'redstone_wire') drops.push('redstone')
-      if (idParsed === 'tripwire') drops.push('string')
-      if (idParsed === 'melon_stem') drops.push('melon_seeds')
-      if (idParsed === 'pumpkin_stem') drops.push('pumpkin_seeds')
-      if (idParsed === 'beetroots') drops.push('beetroot')
-      if (idParsed === 'carrots') drops.push('carrot')
-      if (idParsed === 'cocoa') drops.push('cocoa_beans')
-      if (idParsed === 'kelp_plant') drops.push('kelp')
-      if (idParsed === 'tall_seagrass') drops.push('seagrass')
+      if (idParsed === 'potatoes') drops.push(findItemByName('potato'))
+      if (idParsed === 'redstone_wire') drops.push(findItemByName('redstone'))
+      if (idParsed === 'tripwire') drops.push(findItemByName('string'))
+      if (idParsed === 'melon_stem') drops.push(findItemByName('melon_seeds'))
+      if (idParsed === 'pumpkin_stem') drops.push(findItemByName('pumpkin_seeds'))
+      if (idParsed === 'beetroots') drops.push(findItemByName('beetroot'))
+      if (idParsed === 'carrots') drops.push(findItemByName('carrot'))
+      if (idParsed === 'cocoa') drops.push(findItemByName('cocoa_beans'))
+      if (idParsed === 'kelp_plant') drops.push(findItemByName('kelp'))
+      if (idParsed === 'tall_seagrass') drops.push(findItemByName('seagrass'))
     }
 
     blockData.drops = drops
