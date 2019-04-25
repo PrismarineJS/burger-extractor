3d47592937633cd48872fc25f11162c0a35314f1
Tim timmyRS Speckhals <github@timmyrs.de>
2019-04-25 20:29:35 +0200

    Fix issues with items

--- src/extractors/items.js
+++ src/extractors/items.js
@@ -13,12 +13,62 @@ module.exports = ({ items }, outputDirectory) => new Promise((resolve, reject) =
   for (let name in items.item) {
     const item = items.item[name]
 
-    const itemData = {
+    let itemData = {
       id: item.numeric_id,
       displayName: item.display_name,
       name: item.text_id,
       stackSize: item.max_stack_size
     }
+
+    // Manual fixes
+    switch(itemData.name)
+	{
+		case "music_disc_13":
+		itemData.displayName = "13 Disc"
+		break;
+		case "music_disc_cat":
+		itemData.displayName = "Cat Disc"
+		break;
+		case "music_disc_blocks":
+		itemData.displayName = "Blocks Disc"
+		break;
+		case "music_disc_chirp":
+		itemData.displayName = "Chirp Disc"
+		break;
+		case "music_disc_far":
+		itemData.displayName = "Far Disc"
+		break;
+		case "music_disc_mall":
+		itemData.displayName = "Mall Disc"
+		break;
+		case "music_disc_mellohi":
+		itemData.displayName = "Mellohi Disc"
+		break;
+		case "music_disc_stal":
+		itemData.displayName = "Stal Disc"
+		break;
+		case "music_disc_strad":
+		itemData.displayName = "Strad Disc"
+		break;
+		case "music_disc_ward":
+		itemData.displayName = "Ward Disc"
+		break;
+		case "music_disc_11":
+		itemData.displayName = "11 Disc"
+		break;
+		case "music_disc_wait":
+		itemData.displayName = "Wait Disc"
+		break;
+		case "trident":
+		itemData.stackSize = 1
+		break;
+		default:
+		const len = itemData.name.length
+		if(itemData.name.substr(len - 7) == "_helmet" || itemData.name.substr(len - 11) == "_chestplate" || itemData.name.substr(len - 9) == "_leggings" || itemData.name.substr(len - 6) == "_boots")
+		{
+			itemData.stackSize = 1;
+		}
+	}
 
     extracted.push(itemData)
   }
